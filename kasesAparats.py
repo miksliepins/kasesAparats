cce4768dcce0299883c61ea3d895b8ec8d959681
miksliepins <133051381+miksliepins@users.noreply.github.com>
2025-01-24 12:05:17 +0200

    Update kasesAparats.py

--- kasesAparats.py
+++ kasesAparats.py
@@ -6,23 +6,40 @@ logs.geometry("800x700")
 logs.configure(bg='#E5E5E5')
 
 
-lietotaja_preces = []
-
-def lietotaja_izvēle():
-    pass
-
-
 def augļi_darzeņi_logs():
     
     global ābols
-    
+    global Bumbieris
+    global Apelsīns
+    global Mandarīni
+    global Sīpoli
+    global Tomāts
+    global Gurķis
+    global Paprika
+
+
     auglu_darzenuLogs = Toplevel()
     auglu_darzenuLogs.geometry('500x400')
     auglu_darzenuLogs.title('Augļi un dārzeņi')
-    ābols = Button(auglu_darzenuLogs, text='Ābols',fg='white', bg='#3b3b3b', width=8, height=2, command=lietotaja_izvēle)
-    ābols.place(x= 40, y= 20)
-    
+    ābols = Button(auglu_darzenuLogs, text='Ābols',fg='white', bg='#3b3b3b', width=12, height=2, command=lambda: lietotaja_izvēle("Ābols"))
+    ābols.place(x= 80, y= 50)
+    Bumbieris = Button(auglu_darzenuLogs, text='Bumbieris', fg='white', bg='#3b3b3b', width=12, height=2, command=lambda: lietotaja_izvēle("Bumbieris"))
+    Bumbieris.place(x= 80, y= 100)
+    Apelsīns = Button(auglu_darzenuLogs, text='Apelsīns', fg= 'white', bg= '#3b3b3b', width=12, height= 2, command=lambda: lietotaja_izvēle("Apelsīns"))
+    Apelsīns.place(x= 80, y= 150)
+    Mandarīni = Button(auglu_darzenuLogs, text='Mandarīni', fg='white', bg='#3b3b3b', width=12, height=2, command= lambda: lietotaja_izvēle("Mandarīni"))
+    Mandarīni.place(x= 80, y= 200)
+    Sīpoli = Button(auglu_darzenuLogs, text='Sīpoli', fg='white', bg='#3b3b3b', width=12, height=2, command= lambda: lietotaja_izvēle("Sīpoli"))
+    Sīpoli.place(x= 270, y= 50)
+    Tomāts = Button(auglu_darzenuLogs, text='Tomāts', fg='white', bg='#3b3b3b', width=12, height=2, command= lambda: lietotaja_izvēle("Tomāts"))
+    Tomāts.place(x= 270, y= 100)
+    Gurķis = Button(auglu_darzenuLogs, text='Gurķis', fg='white', bg='#3b3b3b', width=12, height=2, command= lambda: lietotaja_izvēle("Gurķis"))
+    Gurķis.place(x= 270, y= 150)
+    Paprika = Button(auglu_darzenuLogs, text='Paprika', fg='white', bg='#3b3b3b', width=12, height=2, command= lambda: lietotaja_izvēle("Paprika"))
+    Paprika.place(x= 270, y=200)
 
+
+    
 ####################################### pogas
 augli_darzeni_poga = Button(logs, text='Augļu un dārzeņi', fg='white', bg='#3b3b3b', width=30, height=3, command=augļi_darzeņi_logs)
 gaļas_poga = Button(logs, text='Gaļas', fg='white', bg='#3b3b3b', width=30, height=3)
@@ -43,8 +60,17 @@ piena_produkti_poga.place(x= 85, y= 340)
 kases_aprāts_ekrāns = Text(logs, width=40, height= 15, bg= 'lightblue', font='Helvetica', padx= 10, pady=10)
 kases_aprāts_ekrāns.place(x= 350, y= 55)
 
-for produkts in lietotaja_preces:
+ritinātājs = Scrollbar(logs)
+
+lietotaja_preces = []
+
+def lietotaja_izvēle(produkts):
+    lietotaja_preces.append(produkts)
     kases_aprāts_ekrāns.insert(END, produkts + "\n")
+
+kases_aprāts_ekrāns.config()
+
+
 
 
 poga_1 = Button(logs, text=9, bg='lightgrey',padx=20, pady=20)
